43e20e9ebe261639d7e985bdc32ca5f9b19326d3
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2024-04-03 11:16:08 -0700

    stack implementation

---
+++ PA 7/PA 7/Stack.cpp
@@ -0,0 +1,27 @@
+#include "Stack.hpp"
+
+string Stack::peek()
+{
+	return this->mDatesAbsent.back();
+}
+
+string Stack::pop()
+{
+	string date = "";
+	if (!isEmpty())
+	{
+		date = this->mDatesAbsent.back();
+		this->mDatesAbsent.pop_back();
+	}
+	return date;
+}
+
+void Stack::push(string newDate)
+{
+	this->mDatesAbsent.push_back(newDate);
+}
+
+bool Stack::isEmpty()
+{
+	return this->mDatesAbsent.empty();
+}
