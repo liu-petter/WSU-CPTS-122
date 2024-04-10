2ea5f0e5f7c3f275b374a52a241772515d071d57
Peter Liu <peter9342@gmail.com>
2024-04-10 12:36:10 -0700

    Set up class Node

--- PA 8/PA 8/Node.cpp
+++ PA 8/PA 8/Node.cpp
@@ -1,1 +1,48 @@
 #include "Node.hpp"
+
+Node::Node(const string& newData)
+{
+	this->mData = newData;
+	this->mpLeft = nullptr;
+	this->mpRight = nullptr;
+}
+
+Node::~Node()
+{
+	
+}
+
+void Node::setData(const string& newData)
+{
+	this->mData = newData;
+}
+
+string Node::getData(void) const
+{
+	return this->mData;
+}
+
+void Node::setLeft(Node* newLeft)
+{
+	this->mpLeft = newLeft;
+}
+
+Node* Node::getLeft(void) const
+{
+	return this->mpLeft;
+}
+
+void Node::setRight(Node* newRight)
+{
+	this->mpRight = newRight;
+}
+
+Node* Node::getRight(void) const
+{
+	return this->mpRight;
+}
+
+void Node::printData()
+{
+	cout << this->mData << endl;
+}
