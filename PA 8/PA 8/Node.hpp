42f919978100d497cb3e4d4badca750efe058b35
Peter Liu <peter9342@gmail.com>
2024-04-10 12:36:50 -0700

    set up class Node pt2
    i forgor to save :(

--- PA 8/PA 8/Node.hpp
+++ PA 8/PA 8/Node.hpp
@@ -3,5 +3,20 @@
 class Node
 {
 public:
-private:
+	Node(const string& newData);
+	virtual ~Node();
+
+	void setData(const string& newData);
+	string getData(void) const;
+	void setLeft(Node* newLeft);
+	Node* getLeft(void) const;
+	void setRight(Node* newRight);
+	Node* getRight(void) const;
+
+	virtual void printData() = 0;
+
+protected:
+	string mData;
+	Node* mpLeft;
+	Node* mpRight;
 };
